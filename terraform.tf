970efa29606409cfb204cbc6360d668d5a49ec07
vivien-ig <vee.igweze@gmail.com>
2026-09-15 22:01:53 -0400

    updated file

--- terraform.tf
+++ terraform.tf
@@ -6,12 +6,5 @@ terraform {
     }
   }
 
-
-backend "s3" {
-    bucket = "metroc-terraform-state-2"
-    key    = "env/dev/terraform-base.tfstate"
-    region = "ca-central-1"
-  }
-
   required_version = ">= 1.2"
 }
